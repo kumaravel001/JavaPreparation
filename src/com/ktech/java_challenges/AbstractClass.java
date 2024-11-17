2177ade0e6de856c41101b844c250f3512766f29
kumaravel001 <kumaravel140795@gmail.com>
2024-11-17 09:27:00 +0000

    Abstract Class Example

---
+++ src/com/ktech/java_challenges/AbstractClass.java
@@ -0,0 +1,30 @@
+package com.ktech.java_challenges;
+
+public abstract class  AbstractClass {
+    String title;
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public  abstract void setTitle(String title);
+}
+
+class MyBook extends AbstractClass
+{
+    @Override
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+}
+
+class Main
+{
+    public static void main(String[] args) {
+        MyBook newNovel = new MyBook();
+        newNovel.setTitle("A tale of two cities");
+        System.out.println("The title is: "+newNovel.getTitle());
+    }
+}
